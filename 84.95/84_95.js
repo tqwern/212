6380416152f9010190175cc9c830deaea8c83623
tqwern <114398693+tqwern@users.noreply.github.com>
2023-04-05 17:34:33 +0500

    Add files via upload

---
+++ 84.95/84_95.js
@@ -0,0 +1,202 @@
+"use strict";
+// 84
+// 1
+// let num = 3;		
+// if (num > 5 && (num < 10 || num == 20)) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 2
+// let num = 3;	
+// if ((num > 5 || num > 0) && num < 3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 3
+// let num = 3;
+// if ((num == 9 || num > 10) && (num < 20 || num > 20) && num < 30) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 85
+// 1
+// let num1 = 3
+// let num2 = 6
+// if (!(num1 > 0 && num2 < 10)) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 86
+// 1
+// let test = true;
+// if (test === true) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 2 
+// let test = false;
+// if (test === false) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 88
+// 1
+// let test = true;
+// if (test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 89
+// 1
+// let test = true;
+// if (!test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 2 
+// let test = true;
+// if (!test) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 3
+// let test = true;
+// if (!false) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 91
+// 1 
+// let test1 = true;
+// let test2 = true;
+
+// if (test1 && test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 2
+// let test1 = true;
+// let test2 = true;
+// if (test1 && test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 3
+// let test1 = true;
+// let test2 = true;
+
+// if (!test1 && !test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 4
+// let test1 = true;
+// let test2 = true;
+// if (!test1 && test2) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 5
+// let test1 = true;
+// let test2 = true;
+// let test3 = true;
+// if (test1 && test2 && test3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 6
+// let test1 = true;
+// let test2 = true;
+// let test3 = true;
+// if (test1  || test2  && test3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 7 
+// let test1 = true;
+// let test2 = true;
+// let test3 = true;
+// if (test1 || !test2 && !test3) {
+// 	console.log('+++');
+// } else {
+// 	console.log('---');
+// }
+
+// 92
+// 1
+// let test = 10;
+// if (test == 10) {
+// 	console.log('yes');
+// }
+
+// 93
+// 1
+// if (test > 0) console.log('+++'); else console.log('---');
+// 2
+// if (test > 0) console.log('+++');
+
+// 94
+// 1
+// let day = 11;
+// if (day <= 10) {
+// 	console.log('первая');
+// } else if (day <= 20) {
+// 	console.log('вторая');
+// } else if (day <= 31) {
+// 	console.log('третья');
+// }
+
+// 2
+// let day = 32;
+// if (day <= 10) {
+// 	console.log('первая');
+// } else if (day <= 20) {
+// 	console.log('вторая');
+// } else if (day <= 31) {
+// 	console.log('третья'); 
+// } else {
+// 	console.log('неверное значение переменной day');
+// }
+
+// 95
+// 1 
+// let num = 99;
+// if (num <= 99) {
+// 	if (num >= 10) {
+// 		console.log('сумма цифр двухзначна');
+// 	} else {
+// 		console.log('сумма цифр однозначна');
+// 	}
+// }
---
+++ 96.109/96_109.js
@@ -0,0 +1,24 @@
+"use strict";
+// 96
+// 1
+// let lang = 'ru';
+// switch (lang) {
+// 	case 'ru':
+// 		console.log('рус');
+// 	break;
+// 	case 'en':
+// 		console.log('анг');
+// 	break;
+// 	case 'de':
+// 		console.log('нем');
+// 	break;
+// 	default:
+// 		console.log('язык не поддерживается');
+// 	break;
+// }
+
+
+let num = 1;
+let res = num >= 0 ? '1': '2';
+console.log(num);
+
